96e3cf089d189be43282dfb7ebd4cdb0d8da4fe0
dirision <nick.dirisio@mail.utoronto.ca>
2019-07-23 01:02:58 -0400

    some work,  needs to keep goin

--- Discord_Correction_Bot.py
+++ Discord_Correction_Bot.py
@@ -1,7 +1,12 @@
 import corrector
 def main():
+    lineA=""
+    lineB=""
     print("Taking stdin")
-
+    while(True):
+        lineA = raw_input("Input line: ")
+        lineB = raw_input("Correction you want to make: ")
+        c = Corrector()
 
 if __name__ == "__main__":
     main()
--- corrector.py
+++ corrector.py
@@ -1,13 +1,57 @@
-class corrector():
+class Corrector:
     """Contains the functionailty for the correction bot"""
     _line = ""
     _correction = ""
+    _lineDict={}
 
-    def corrector():
+    def __init__(self):
         print("Building corrector\nNo Args Given")
     
-    def corrector(inputLine):
+    def __init__(self,inputLine):
         print("Building corrector\nLine Arg Given")
-        _line=inputLine
+        self._line=inputLine
+        self.generateNewDictionary()
 
-           
+    def __init__(self,inputLine, correctionLine):
+        print("Building corrector\nLine Arg Given")
+        self._line=inputLine
+        self._correction=correctionLine
+        self.generateNewDictionary()
+
+    def generateNewDictionary(self):
+        for word in self.inputLine:
+            self._lineDict[word] = 0.0
+
+    def newCorrection(self,new):
+        self._correction=new
+
+    '''
+    Returns the sentance with the correction
+    '''
+    def getCorrection(self):
+        word=findWord()
+        
+        
+    '''
+    Find the word within the sentance that relates most to the 'corrector' provided
+    The value associated with each key is how close the correction is to the word 
+    as a percentage 
+    ex:
+    lineA: this is a sentanec 
+    lineB: sentance*
+    dict: 
+        this-2.4
+        is-50
+        a-0
+        sentance-93.22
+
+    calculation: 
+        27 is the length of the longest word in the english dictionary 
+        Therefore, the maximum distance between the longest word and 
+        the shortest word is 26 characters
+        score = abs( 26 - abs(corr - word))/26
+    '''
+    def findWord():
+        pass
+
+
