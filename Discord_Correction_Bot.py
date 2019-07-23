df546417828b8f2e19754b0043a3c0d4e05ef6b5
Nicholas DiRisio <nick.dirisio@mail.utoronto.ca>
2019-07-22 23:19:19 -0400

    init

---
+++ Discord_Correction_Bot.py
@@ -0,0 +1,7 @@
+import corrector
+def main():
+    print("Taking stdin")
+
+
+if __name__ == "__main__":
+    main()
---
+++ corrector.py
@@ -0,0 +1,13 @@
+class corrector():
+    """Contains the functionailty for the correction bot"""
+    _line = ""
+    _correction = ""
+
+    def corrector():
+        print("Building corrector\nNo Args Given")
+    
+    def corrector(inputLine):
+        print("Building corrector\nLine Arg Given")
+        _line=inputLine
+
+           
