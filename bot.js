17b902ca426c210a3248f28fc1e02760de41bdb3
dirision <nick.dirisio@mail.utoronto.ca>
2019-08-01 00:45:46 -0400

    kinda working, can loop through previous messages now

--- bot.js
+++ bot.js
@@ -3,13 +3,16 @@ var logger = require('winston');
 var auth = require('./auth.json');
 
 
-
-
 // configure logger
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, { colorize: true });
 logger.level = 'debug';
 
+// returns the second last message from userID from a list of messages
+function getSecondLastMessage(userID, messages) {
+
+
+}
 // init bot
 var bot = new Discord.Client({
     token: auth.token,
@@ -22,7 +25,7 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + '-(' + bot.id + ')');
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', function (user, userID, chanID, message, evt) {
     // parse and run depending on input
     if (message.slice(-1) == '*') {
         var args = message.substring(0, message.length - 1);
@@ -36,11 +39,31 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch (cmd) {
             case "*":
                 bot.sendMessage({
-                    to: channelID,
+                    to: chanID,
                     message: 'Hi, Im online!\nTo use me, add an * to the end of the word you want to correct from a previous message'
                     
                 });
                 break;
+            default:
+                bot.sendMessage({
+                    to: chanID,
+                    message: 'Received message: ' + args + " from: " + userID + "\nLooking for previous message"
+
+                });
+                // pull message array 
+                messages = bot.getMessages({
+                    channelID: chanID,
+                    limit: 20
+                }, function (error, response) {
+                    for (var i in response) {
+
+                        logger.info(response[i]['content']);
+                    }
+                });
+                logger.info("Last 20 messages: " + messages);
+                // getSecondLastMessage(userID);
+                break;
         }
     }
 });
+
